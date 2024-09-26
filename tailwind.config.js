811fe2db4932e68cd6180402b766b70a362a12d0
manasrathi28 <107744607+manasrathi28@users.noreply.github.com>
2024-09-27 03:42:59 +0530

    intial changes

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        darkBackground: '#0b0b0d', // Darker custom background
+        darkPrimary: '#0f3460', // Custom primary color
+        lightText: '#e94560', // Custom light text color
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
